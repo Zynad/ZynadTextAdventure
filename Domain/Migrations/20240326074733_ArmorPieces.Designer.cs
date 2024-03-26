c24bc9565b5d419f06c614d6166b8ece945f459b
Alexander Adolfsson <alexander.adolfsson@zynad.se>
2024-03-26 08:53:16 +0100

    Added migration for Armor pieces

--- Domain/Migrations/20240326074733_ArmorPieces.Designer.cs
+++ Domain/Migrations/20240326074733_ArmorPieces.Designer.cs
@@ -4,6 +4,7 @@ using Domain.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Domain.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20240326074733_ArmorPieces")]
+    partial class ArmorPieces
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Domain/Migrations/20240326074733_ArmorPieces.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class ArmorPieces : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Boots",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LevelRequirement = table.Column<int>(type: "int", nullable: false),
+                    Rarity = table.Column<int>(type: "int", nullable: false),
+                    Value = table.Column<int>(type: "int", nullable: false),
+                    Weight = table.Column<int>(type: "int", nullable: false),
+                    Durability = table.Column<int>(type: "int", nullable: false),
+                    Material = table.Column<int>(type: "int", nullable: false),
+                    ArmorValue = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Boots", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Chest",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LevelRequirement = table.Column<int>(type: "int", nullable: false),
+                    Rarity = table.Column<int>(type: "int", nullable: false),
+                    Value = table.Column<int>(type: "int", nullable: false),
+                    Weight = table.Column<int>(type: "int", nullable: false),
+                    Durability = table.Column<int>(type: "int", nullable: false),
+                    Material = table.Column<int>(type: "int", nullable: false),
+                    ArmorValue = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Chest", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Gloves",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LevelRequirement = table.Column<int>(type: "int", nullable: false),
+                    Rarity = table.Column<int>(type: "int", nullable: false),
+                    Value = table.Column<int>(type: "int", nullable: false),
+                    Weight = table.Column<int>(type: "int", nullable: false),
+                    Durability = table.Column<int>(type: "int", nullable: false),
+                    Material = table.Column<int>(type: "int", nullable: false),
+                    ArmorValue = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Gloves", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Helmet",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LevelRequirement = table.Column<int>(type: "int", nullable: false),
+                    Rarity = table.Column<int>(type: "int", nullable: false),
+                    Value = table.Column<int>(type: "int", nullable: false),
+                    Weight = table.Column<int>(type: "int", nullable: false),
+                    Durability = table.Column<int>(type: "int", nullable: false),
+                    Material = table.Column<int>(type: "int", nullable: false),
+                    ArmorValue = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Helmet", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Legs",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LevelRequirement = table.Column<int>(type: "int", nullable: false),
+                    Rarity = table.Column<int>(type: "int", nullable: false),
+                    Value = table.Column<int>(type: "int", nullable: false),
+                    Weight = table.Column<int>(type: "int", nullable: false),
+                    Durability = table.Column<int>(type: "int", nullable: false),
+                    Material = table.Column<int>(type: "int", nullable: false),
+                    ArmorValue = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Legs", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Boots");
+
+            migrationBuilder.DropTable(
+                name: "Chest");
+
+            migrationBuilder.DropTable(
+                name: "Gloves");
+
+            migrationBuilder.DropTable(
+                name: "Helmet");
+
+            migrationBuilder.DropTable(
+                name: "Legs");
+        }
+    }
+}
