007c8a16b23a21ab7e4aececadb0e1791e38eff9
Zynad <97847547+Zynad@users.noreply.github.com>
2025-12-18 20:11:06 +0100

    Refine vocation defaults for frontend

--- ApplicationServices/Classes/Mage.cs
+++ ApplicationServices/Classes/Mage.cs
@@ -1,11 +1,11 @@
-﻿using ApplicationServices.Game.Helpers;
-using ApplicationServices.Items.Equipment.Armor;
+﻿using ApplicationServices.Items.Equipment.Armor;
 using ApplicationServices.Items.Equipment.Weapons;
 using ApplicationServices.PlayerSettings;
 using ApplicationServices.Services.Armor;
 using ApplicationServices.Services.Weapons.WeaponServices;
 
 namespace ApplicationServices.Classes;
+
 public class Mage : Vocation
 {
     private readonly IWandService _wandService;
@@ -15,6 +15,14 @@ public class Mage : Vocation
     private readonly IGlovesService _glovesService;
     private readonly IHelmetService _helmetService;
     private readonly ILegsService _legsService;
+
+    private const string DefaultStaffName = "Oak Quarterstaff";
+    private const string DefaultBootsName = "Mystic Sandals";
+    private const string DefaultChestName = "Mystic Robes";
+    private const string DefaultGlovesName = "Battlemage Wraps";
+    private const string DefaultHelmetName = "Mystic Circlet";
+    private const string DefaultLegsName = "Mystic Legwraps";
+
     public Mage(IWandService wandService, IStaffService staffService, IBootsService bootsService, IChestService chestService, IGlovesService glovesService, IHelmetService helmetService, ILegsService legsService)
     {
         _wandService = wandService ?? throw new ArgumentNullException(nameof(wandService));
@@ -30,49 +38,24 @@ public class Mage : Vocation
     public override async Task SetBaseValues(Player player)
     {
         var allowedArmor = new List<ArmorMaterial> { ArmorMaterial.Cloth };
-        var allowedWeapon = new List<WeaponType> { WeaponType.Staff , WeaponType.Wand};
+        var allowedWeapon = new List<WeaponType> { WeaponType.Staff, WeaponType.Wand };
+
         player.SetBaseValues(30, 10, 5, 50, 30, 15, 10, 30, allowedArmor, allowedWeapon);
-        await ChooseWeapon(player);
-        await ChooseEquipment(player);
-    }
-    private async Task ChooseWeapon(Player player)
-    {
-        WeaponBase? startingWeapon = null;
-        while (startingWeapon == null)
-        {
-            string weaponChoice = ParseHelper.AskForString("Game Master : Choose your starting weapon:\n1. Staff\n2. Wand\n");
-            startingWeapon = await CreateWeapon(weaponChoice);
-            if (startingWeapon == null)
-            {
-                Console.WriteLine("Game Master : Invalid choice");
-            }
-        }
-        player.MainHand = startingWeapon;
-        Console.WriteLine($"Game Master : You have chosen a {player.MainHand.Name} as your starting weapon.");
+        await EquipDefaultWeapon(player);
+        await EquipDefaultArmor(player);
     }
 
-    private async Task ChooseEquipment(Player player)
+    private async Task EquipDefaultWeapon(Player player)
     {
-        player.Boots = await _bootsService.GetBoots(x => x.Name == "Beginner Sandals");
-        player.Chest = await _chestService.GetChest(x => x.Name == "Beginner Robe");
-        player.Gloves = await _glovesService.GetGlove(x => x.Name == "Beginner Cloth Gloves");
-        player.Helmet = await _helmetService.GetHelmet(x => x.Name == "Beginner Cloth Helmet");
-        player.Legs = await _legsService.GetLeg(x => x.Name == "Beginner Cloth Legs");
-        Console.WriteLine("Game Master : Here is also a set of cloth armor that you should wear, you can inspect them further later on");
-        Console.WriteLine($"{player.FirstName} is getting dressed");
-        // Wait for 3 seconds
-        await Task.Delay(TimeSpan.FromSeconds(3));
-        Console.WriteLine("Game Master : They look so good on you!");
+        player.MainHand = await _staffService.GetWeapon(x => x.Name == DefaultStaffName);
     }
 
-    private async Task<WeaponBase?> CreateWeapon(string weaponChoice)
+    private async Task EquipDefaultArmor(Player player)
     {
-        return weaponChoice switch
-        {
-            "staff" or "1" => await _staffService.GetWeapon(x => x.Name == "Beginner Staff"),
-            "wand" or "2" => await _wandService.GetWeapon(x => x.Name == "Beginner Wand"),
-            _ => null
-        };
+        player.Boots = await _bootsService.GetBoots(x => x.Name == DefaultBootsName);
+        player.Chest = await _chestService.GetChest(x => x.Name == DefaultChestName);
+        player.Gloves = await _glovesService.GetGlove(x => x.Name == DefaultGlovesName);
+        player.Helmet = await _helmetService.GetHelmet(x => x.Name == DefaultHelmetName);
+        player.Legs = await _legsService.GetLeg(x => x.Name == DefaultLegsName);
     }
-
 }
